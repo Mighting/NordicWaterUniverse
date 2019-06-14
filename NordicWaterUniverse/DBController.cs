838122e0e2845f1a7a63ed25ca95854760e9c49d
Rasmus Erik Seremet Rosenkjær <zbcraros2@zbc.dk>
2019-06-14 13:05:13 +0200

    Connection and Wrtie to DB is now done. Perhaps put on some restrictions or go to App development

--- NordicWaterUniverse/DBController.cs
+++ NordicWaterUniverse/DBController.cs
@@ -12,10 +12,14 @@ namespace NordicWaterUniverse
     {
         private static DBController Dbc_Instance = new DBController();
 
+        SqlConnection connection = new SqlConnection("Data Source=ZBC-E-ROMA-1843;Initial Catalog=NordicWaterUniverseDB;Integrated Security=True");
+        SqlCommand command;
+
         string newScanChipId;
 
         private DBController()
         {
+
             ComPortListener.getInstance().newScan += DBController_AddtoDB;
         }
 
@@ -24,8 +28,11 @@ namespace NordicWaterUniverse
             return Dbc_Instance;
         }
 
+
         private void DBController_AddtoDB(object sender, EventArgs e)
         {
+            OpenSQLConnection();
+
             //Add to database here
             //We check to see what E is, and then we parse it into our own EventArgs to get the ChipIdNumber and put that into our own ChipId
             if (e is CheckInEventArgs)
@@ -33,8 +40,21 @@ namespace NordicWaterUniverse
                 CheckInEventArgs ch = (CheckInEventArgs)e;
                 newScanChipId = ch.ChipIdNumber;
             }
+
+            command = new SqlCommand($"INSERT INTO CheckedIn VALUES ({newScanChipId})", connection);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+
         }
 
-        SqlConnection sqlConnection = new SqlConnection();
+        public void OpenSQLConnection()
+        {
+            connection.Open();
+            Console.WriteLine("Connection to Database is now Open");
+        }
+
     }
+
 }
